43769e8bb7241619f17aac2164a73774cd85172e
Daniel Castillo <danielcastillog92@gmail.com>
2021-05-13 12:00:15 +0200

    Login style added.

--- prueba-gana-front/gana-energia/src/Containers/Login/Login.jsx
+++ prueba-gana-front/gana-energia/src/Containers/Login/Login.jsx
@@ -4,7 +4,16 @@ import './Login.css';
 const Login = () => {
     return (
         <div className='loginContainer'>
-            
+            <div className='loginForm'>
+                <div className='nickname'>
+                    Nickname
+                    <input></input>
+                </div>
+                <div className='password'>
+                    Password
+                    <input type='password'></input>
+                </div>
+            </div>
         </div>
     )
 }
